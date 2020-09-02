8d520cc7cf81ab8f7d35281c1a85056a4962463a
nairMadhav <maddy.nair01@gmail.com>
2020-09-02 15:44:31 +0530

    add first GET route

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const app = express();
 
+//middleware
+const mockUserData=[{name:'Madhav'},{name:'Nair'}]
+app.get('/users',function(req,res){
+    res.json({
+        success:true,
+        message:'successfull acquired users...',
+        users:mockUserData
+    })
+})
+
 app.listen(8000,function(){
     console.log('server is running')
 })
